4cb5a44fc4706603f6a5069c2ddf197769db2c42
chokh <chokh1443@gmail.com>
2016-06-04 23:00:58 +0900

    no message

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -11,10 +11,59 @@ public class Controller implements IController{
 	public Controller(FileModel left, FileModel right, View v ){
 		this.left = left;
 		this.right = right;
-		this.view = v;
+		this.view = view View(this);		
+		
+		view.make();
 	}
 	
+	public void eventOccured(String asd){
+		if(asd.equals("load")){
+			loadLeft();
+		}
+	}
 	
+	@Override
+	public void loadLeft() {
+		String fileAddress = "";
+		
+		if( left.loadData("") )
+			 view.showData(left.DataToAString());
+		else
+			System.out.println("Fail to load at Controller.loadLeft.left.loadData()");
+		
+		// TODO Auto-generated method stub
+		
+		
+	}
 
+	@Override
+	public void saveLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editRight() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
