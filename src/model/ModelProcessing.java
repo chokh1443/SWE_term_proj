f4a7b58dfa8a08104e9cc3c051eba730f8b1bd49
Kim Hodong <jadong20@gmail.com>
2016-06-10 00:11:41 +0900

    컴패어 개행 한번만 일어나게  수정

--- src/model/ModelProcessing.java
+++ src/model/ModelProcessing.java
@@ -1,12 +1,13 @@
 package model;
 
-public class ModelProcessing implements IModelProcessing{
+public class ModelProcessing implements IModelProcessing {
 	FileModel left;
 	FileModel right;
 	int[][] lcs; // lcs 알고리즘 수행 결과
 	int[] areaLeft, areaRight; // lcs 알고리즘 수행 결과를 바탕으로 구역을 나눔.
-	
-	public ModelProcessing(FileModel left, FileModel right){
+	boolean compared = false;
+
+	public ModelProcessing(FileModel left, FileModel right) {
 		this.left = left;
 		this.right = right;
 	}
@@ -14,113 +15,130 @@ public class ModelProcessing implements IModelProcessing{
 	@Override
 	public void compare() {
 		// TODO Auto-generated method stub
-		int l=0, r=0;
-		int leftLength = left.Data.size(), rightLength = right.Data.size();
-		lcs = new int[leftLength+1][rightLength+1];
+		if (!compared) {
 
-		
-		for(l = leftLength-1; l >=0  ; l--){ // 최종값은 lcs[leftLength][rightLength] 에 저장.
-			for(r = rightLength-1; r >= 0 ; r--){
-				if( left.Data.get(l).equals( right.Data.get(r) ) ){
-						lcs[l][r] = lcs[l+1][r+1] + 1;						
-				}else{
-					lcs[l][r] = Math.max(lcs[l][r+1], lcs[l+1][r]);
+			int l = 0, r = 0;
+			int leftLength = left.Data.size(), rightLength = right.Data.size();
+			lcs = new int[leftLength + 1][rightLength + 1];
+
+			for (l = leftLength - 1; l >= 0; l--) { // 최종값은
+													// lcs[leftLength][rightLength]
+													// 에 저장.
+				for (r = rightLength - 1; r >= 0; r--) {
+					if (left.Data.get(l).equals(right.Data.get(r))) {
+						lcs[l][r] = lcs[l + 1][r + 1] + 1;
+					} else {
+						lcs[l][r] = Math.max(lcs[l][r + 1], lcs[l + 1][r]);
+					}
 				}
 			}
-		}
-		
-		
-		areaLeft = new int[ leftLength + rightLength  ]; // worst case의 길이
-		areaRight = new int[ leftLength + rightLength ];
-		
-		for(l = 0; l <= leftLength ; l++){
-			for(r=0; r <= rightLength ; r++){
-				System.out.print(" " + lcs[l][r]);
-			}
-			System.out.println("");
-		}
-		
-		l = 0;
-		r = 0;	
-		int temp;
-		int tl=0;
-		int tr=0;
-		
-		while(true){
-			temp = lcs[l][r];
-			
-			if(temp == lcs[l][r+1]){
-			//	System.out.println("right");
-				areaLeft[tl] = 0; 			//	areaL 공백
-				left.Data.add(tl,"");
-				tl++;
-				areaRight[tr] = 3;			//	areaR 라인
-				tr++;
-				
-				r++;
 
-			}else if(temp == lcs[l+1][r]){
-			//	System.out.println("down");
-				areaLeft[tl] = 1; 			//	areaL 라인
-				tl++;
-				areaRight[tr] = 0;			//	areaR 공백		
-				right.Data.add(tr,"");
-				tr++;
-				
-				l++;
-			}else{
-			//	System.out.println("diagonal");
-				areaLeft[tl] = 2;			//	areaL 라인
-				tl++;
-				areaRight[tr] = 2;			//	areaR 라인
-				tr++;
-				
-				l++;
-				r++;
+			System.out.println("LCS completed");
+
+			areaLeft = new int[leftLength + rightLength]; // worst case의 길이
+			areaRight = new int[leftLength + rightLength];
+
+			for (l = 0; l <= leftLength; l++) {
+				for (r = 0; r <= rightLength; r++) {
+					System.out.print(" " + lcs[l][r]);
+				}
+				System.out.println("");
 			}
+
+			l = 0;
+			r = 0;
+			int temp;
+			int tl = 0;
+			int tr = 0;
+			String blank = "";
 			
-			
-			if( (l == leftLength) && (r < rightLength)){// areaR 남은라인 추가
-				while(true){
-					areaRight[tr] = 3;
-					tr++;
-					left.Data.add(tl,"");
+			while (true) {
+				temp = lcs[l][r];
+				if (l + 1 > leftLength)
+					break;
+				if (r + 1 > rightLength)
+					break;
+
+				if (temp == lcs[l][r + 1]) {
+					System.out.println("right");
+					areaLeft[tl] = 0; // areaL 공백
+					left.Data.add(tl, blank);
+
 					tl++;
+					areaRight[tr] = 3; // areaR 라인
+					tr++;
+
 					r++;
-					if(r==rightLength)
-						break;
-				}
-				break;
-			}else if( (l < leftLength) && (r == rightLength)){// areaL 남은라인 추가
-				while(true){
-					areaLeft[tl] = 1;
+
+				} else if (temp == lcs[l + 1][r]) {
+					System.out.println("down");
+					areaLeft[tl] = 1; // areaL 라인
 					tl++;
-					right.Data.add(tr,"");
+
+					areaRight[tr] = 0; // areaR 공백
+					right.Data.add(tr, blank);
+
 					tr++;
+
 					l++;
-					if(l==leftLength)
-						break;
+				} else {
+					System.out.println("diagonal");
+					areaLeft[tl] = 2; // areaL 라인
+					tl++;
+					areaRight[tr] = 2; // areaR 라인
+					tr++;
+
+					l++;
+					r++;
 				}
-				break;
-			}			
-		}
-		
-		areaLeft[tl] = 9;
-		areaRight[tr] = 9;
-/*		
-		if(tl > tr){
-			for(int i = 0; i < tl; i++){
-				System.out.println(areaLeft[i] +" "+ areaRight[i]);
+
+				if ((l == leftLength) && (r < rightLength)) {// areaR 남은라인 추가
+					while (true) {
+						areaRight[tr] = 3;
+						tr++;
+
+						// left.Data.add(tl, "");
+						// tl++;
+						r++;
+						if (r == rightLength)
+							break;
+					}
+					tl++;
+					break;
+				} else if ((l < leftLength) && (r == rightLength)) {// areaL
+																	// 남은라인 추가
+					while (true) {
+						areaLeft[tl] = 1;
+						tl++;
+						// right.Data.add(tr, "");
+						// tr++;
+						l++;
+						if (l == leftLength)
+							break;
+					}
+					tr++;
+					break;
+				}
 			}
-		}
-		System.out.println("-------");
-		if(tl<tr){
-			for(int i = 0; i < tr; i++){
-				System.out.println(areaLeft[i] +" "+ areaRight[i]);
+
+			areaLeft[tl] = 9;
+			areaRight[tr] = 9;
+
+			if (tl > tr) {
+				for (int i = 0; i < tl; i++) {
+					System.out.println(areaLeft[i] + " " + areaRight[i]);
+				}
 			}
-		}*/
-		left.setCompared();
-		right.setCompared();
+			System.out.println("-------");
+			if (tl < tr) {
+				for (int i = 0; i < tr; i++) {
+					System.out.println(areaLeft[i] + " " + areaRight[i]);
+				}
+			}
+			left.setCompared();
+			right.setCompared();
+			compared = true;
+		}
 	}
 
 	@Override
@@ -130,6 +148,7 @@ public class ModelProcessing implements IModelProcessing{
 				areaRight[sourceInd] = 2;
 				areaLeft[sourceInd] = 2;
 				right.Data.set(sourceInd, left.Data.get(sourceInd));
+				compared = false;
 			}
 		}
 	}
@@ -141,6 +160,7 @@ public class ModelProcessing implements IModelProcessing{
 				areaRight[sourceInd] = 2;
 				areaLeft[sourceInd] = 2;
 				left.Data.set(sourceInd, right.Data.get(sourceInd));
+				compared = false;
 			}
 		}
 	}
