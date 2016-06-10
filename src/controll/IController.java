56b27fc73a2ab7b2215437dd34c36e7fcd8a662a
Kim Hodong <jadong20@gmail.com>
2016-06-10 20:32:33 +0900

    no message

--- src/controll/IController.java
+++ src/controll/IController.java
@@ -1,12 +1,9 @@
 package controll;
 
-import model.FileModel;
-import view.View;
+
 
 public interface IController {
-	FileModel left = null;
-	FileModel right = null;
-	View view = null;
+
 	
 	abstract void load(String side);
 	abstract void save(String side);
